60ad35297c0e6a34cf9984b647d6ff5bbbed1fc0
Bruno Sesso <bruno.sesso@usp.br>
2014-09-18 21:18:17 -0300

    Arrumei o tester... ta chuchuzinho

--- mini-ep2/EP1/ep.py
+++ mini-ep2/EP1/ep.py
@@ -1,23 +1,32 @@
 import sys
 
+option = ""
+try:
+    option = sys.argv[1]
+except IndexError:
+    print("Passe a opcao do programa como argumento!\nTomando opcao -a como padrão")
+    option = "-a"
+
 names = dict()
-if sys.argv[1] == '-a':
+if option == '-a':
     for line in sys.stdin.readlines():
         if line[0] != '#':
             args = line.split(':')
             name = args[4].split()
             if len(name) == 0:
-                name.append('')
+                name = ''
             elif len(name) == 1:
-                name.append('')
-            name = name[0] + ' ' + name[-1]
+                name = name[0]
+            elif len(name) > 1:
+                name = name[0] + ' ' + name[-1]
+
             user = args[0]
 
             user_list = names.get(name, [])
             user_list.append(user)
             names[name] = user_list
 
-elif sys.argv[1] == '-u':
+elif option == '-u':
     for line in sys.stdin.readlines():
         if line[0] != '#':
             args = line.split(':')
@@ -29,7 +38,7 @@ elif sys.argv[1] == '-u':
             names[uid] = user_list
 
 else:
-    print('Invalid argument')
+    print('Argumento invalido')
 
 for key, value in names.items():
     if len(value) > 1:
