f38889571d4830ca19336657a80dfa7eccb5155e
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2014-09-28 20:31:31 -0300

    solucao lu feita porem seg fault, erros estranhissimos ocorrendo

--- lu/lu_solver.c
+++ lu/lu_solver.c
@@ -4,29 +4,44 @@
 #define abs(x) (((x) < 0) ? -(x) : (x))
 #define eps 1e-16
 
+int leMatriz(double A[][NMAX], double b[NMAX]);
 int lucol(int n, double A[][NMAX], int p[]);
+int sscol(int n, double A[][NMAX], int p[], double b[]);
 void swap(double *a, double *b);
+void printMatrix(double A[][NMAX], int n);
 
 int main(int argc, char **argv) {
-    int p[3];
-    int n = 3;
-    
-    double A[3][3] = {
-         {1.0, 2.0, 3.0},
-         {0.0, 5.0, 6.0},
-         {0.0, 0.0, 9.0}};
+    int n = 0;
+    int i;
+    int p[NMAX];
+    double b[NMAX];
+    double A[NMAX][NMAX];
 
-    lucol(n, A, p);
-
-    printf("\n\n");
-    int i, j;
+    n = leMatriz(A, b);
     for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++)
-            printf("%3.2f ", A[i][j]);
-        printf("\n");
+        printf("%3.2f\t%d\n", b[i], i);
     }
-    
+    printf("WTF IS HAPPENING IN THIS SHIT\n");
+    lucol(n, A, p);
+    for (i = 0; i < n; i++) {
+        printf("%3.2f\t%d\n", b[i], i);
+    }
+    sscol(n, A, p, b);
     return 0;
+}
+
+int leMatriz(double A[][NMAX], double b[NMAX]) {
+    int n, k, i, j;
+    scanf("%d", &n);
+    for (k = 0; k < n * n; k++) {
+        scanf("%d %d", &i, &j);
+        scanf("%lf", &A[i][j]);
+    }
+    for (k = 0; k < n; k++) {
+        scanf("%d", &i);
+        scanf("%lf", &b[i]);
+    }
+    return n;
 }
 
 int lucol(int n, double A[][NMAX], int p[]) {
@@ -38,23 +53,64 @@ int lucol(int n, double A[][NMAX], int p[]) {
             if (abs(A[i][k]) > abs(A[imax][k])) 
                 imax = i;
 
-        if (abs(A[imax][k]) < eps) 
-            printf("\nPARA, pois A parece singular");
+        if (abs(A[imax][k]) < eps)  {
+            printf("Matriz parece ser singular\n");
+            return 0;
+        }
         if (imax != k)
             for (j = 0; j < n; j++)
                 swap(&A[imax][j], &A[k][j]);
         p[k] = imax;
-
         for (i = k + 1; i < n; i++) {
             A[i][k] = A[i][k] / A[k][k];
             for (j = k + 1; j < n; j++)
                 A[i][j] = A[i][j] - A[k][j] * A[i][k];
         }
-    }     
+    }
+    return 1;     
+}
+
+int sscol(int n, double A[][NMAX], int p[], double b[]) {
+    int i, j;
+    double temp;
+    for (i = 0; i < n; i++) {
+        /*printf("p[%d] = %d | temp = %lf, b[%d] = %lf, b[p[%d]] = %lf\n", i, p[i], temp, i, b[i], i, b[p[i]]);*/
+        temp = b[i];
+        b[i] = b[p[i]];
+        b[p[i]] = temp;
+    }
+
+    /*Ly = b*/
+    for (j = 0; j < n; j++) {
+        for (i = j + 1; i < n; i++)
+            b[j] -= A[i][j] * b[i];
+    }
+
+    /*Ux = y*/
+    for (j = n - 1; j >= 0; j--) {
+        if (abs(A[j][j]) < eps) {
+            printf("Matriz parece ser singular\n");
+            return 0;
+        }
+        b[j] = b[j] / A[j][j];
+        for (i = 0; i < j; i++) {
+            b[i] -= b[j] * A[i][j];
+        }
+    }
+    return 1;
 }
 
 void swap(double *a, double *b) {
     double temp = *a;
     *a = *b;
     *b = temp;
+}
+
+void printMatrix(double A[][NMAX], int n) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            printf("%3.2f ", A[i][j]);
+        printf("\n");
+    }
 }
