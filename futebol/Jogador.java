e2f46b78473c1822a70547eaae2dc78865ec6228
gustavoem <estrela.gustavo.matos@gmail.com>
2014-11-23 13:43:36 -0200

    falta so calcular a forca

--- futebol/Jogador.java
+++ futebol/Jogador.java
@@ -2,25 +2,33 @@ package futebol;
 
 public class Jogador {
 
-	private String nome;
-	private double pontuacao;
+    private String nome;
+    private Stats stats;
+    private double pontuacao;
+    private Forca forca;
 
-	public Jogador(String nome, double pontuacao) {
-		this.nome = nome;
-		this.pontuacao = pontuacao;
-	}
+    public Jogador(String nome, Stats stats, CalculaForcaStrategy tipoDeForca) {
+        this.nome = nome;
+        this.pontuacao = pontuacao;
+        this.forca = new Forca(tipoDeForca);
+    }
+    
+    private atualizaPontuacao() {
+        this.potuacao = this.forca.calcForca(this.stats);
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public double getForca() {
-		return pontuacao;
-	}
-
-	public String toString() {
-		StringBuilder string = new StringBuilder();
-		string.append(getNome()).append(" (").append(getForca()).append(")");
-		return string.toString();
-	}
+    public double getForca() {
+        this.atualizaPontuacao();
+        return pontuacao;
+    }
+    
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        string.append(getNome()).append(" (").append(getForca()).append(")");
+        return string.toString();
+    }
 }
--- futebol/Stats.java
+++ futebol/Stats.java
@@ -4,8 +4,60 @@ public class Stats {
 	private int idade;
 	private double altura;
 	private double peso;
-	private int forca;
 	private int velocidade;
 	private int explosao;
 	private int resistencia;
+
+	public Stats(int idade, double altura, double peso, int velocidade, int explosao, int resistencia) {
+		if (idade < 0) {
+			throw new IllegalArgumentException("idade deve ser maior do que zero");
+		}
+		if (altura < 0) {
+			throw new IllegalArgumentException("altura deve ser maior do que zero");
+		}
+		if (peso < 0) {
+			throw new IllegalArgumentException("peso deve ser maior do que zero");
+		}
+		if (velocidade > 5 || velocidade < 0) {
+			throw new IllegalArgumentException("velocidade deve ser um inteiro no intervalo [0, 5]")
+		}
+		if (explosao > 5 || explosao < 0) {
+			throw new IllegalArgumentException("explosao deve ser um inteiro no intervalo [0, 5]")
+		}
+		if (resistencia > 5 || resistencia < 0) {
+			throw new IllegalArgumentException("resistencia deve ser um inteiro no intervalo [0, 5]")
+		}
+
+		this.idade = idade;
+		this.altura = altura;
+		this.peso = peso;
+		this.velocidade = velocidade;
+		this.explosao = explosao;
+		this.resistencia = resistencia;
+	}
+
+	public getIdade() {
+		return idade;
+	}
+
+	public getAltura() {
+		return altura;
+	}
+
+	public getPeso() {
+		return peso;
+	}
+
+	public getVelocidade() {
+		return velocidade;
+	}
+
+	public getExplosao() {
+		return explosao;
+	}
+
+	public getResistencia() {
+		return resistencia;
+	}
 }
+
