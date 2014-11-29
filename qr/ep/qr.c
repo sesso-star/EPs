f2310dd7b361b830bbfd05e79141b6a9776f3f08
Bruno <bruno.sesso@usp.br>
2014-11-29 10:54:15 -0200

    swap qr2.c qr.c

--- qr/ep/qr.c
+++ qr/ep/qr.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX 100
+#define MAX 1000
 #define abs(x) (((x) < 0) ? -(x) : (x))
 
 /** FUNÇÕES PRINCIPAIS **/
-void qr(double A[][MAX], double b[], int n, int m);
-void qr_solve(double A[][MAX], double b[], int m);
-void prepareMatrix(double A[][MAX], int map[], int n, int m);
+void qr(double A[][MAX], double b[], double sigma[], int map[], int n, int m);
+void qr_solve(double A[][MAX], double b[], double sigma[], int m);
+void getColNorms(double A[][MAX], double sigma[], int n, int m);
 
 /** FUNÇÕES AUXILIARES **/
 int readMatrix(double A[][MAX], double b[], int *n, int *m); 
@@ -20,165 +20,169 @@ void swapColumns(double A[][MAX], int n, int col1, int col2);
 void swap(double *a, double *b); 
 
 int main(int argc, char **argv) {
-	int n, m;
-	double A[MAX][MAX];
-	double b[MAX];
-	int map[MAX];
+    int n, m;
+    double A[MAX][MAX];
+    double b[MAX];
+    int map[MAX];
+    double sigma[MAX];
 
-	readMatrix(A, b, &n, &m);
+    readMatrix(A, b, &n, &m);
 
-//	printMatrix(A, n, m);
-//	system("sleep 5");
-	prepareMatrix(A, map, n, m);
-//	printMatrix(A, n, m);
-//	system("sleep 10");
+    getColNorms(A, sigma, n, m);
 
-//  printMatrix(A, n, m);
-//  printVector(b, n);
-
-	qr(A, b, n, m);
-    qr_solve(A, b, m);
-	remap(b, map, n);
+    qr(A, b, sigma, map, n, m);
+    qr_solve(A, b, sigma, m);
+    remap(b, map, m);
 
 //  printMatrix(A, n, m);
-    printVector(b, n);
+//	printVector(sigma, m);
+    printVector(b, m);
 
-	return 0;
+    return 0;
 }
 
 /** FUNÇÕES PRINCIPAIS *****************************************************************/ 
 
-void qr(double A[][MAX], double b[], int n, int m) {
-	int i, j, k;
-	double max, sigma, alpha, beta, gama;
+/* Calcula a decomposição QR de uma dada matriz A.
+ * Recebe a matriz A que será decomposta em Q*R e a Q_i e R encontradas são retornadas 
+ * armazenadas na própria A. Recebe-se o vetor 'b' (Ax = b) e retorna-se nele mesmo
+ * o vetor c = Qt * b. Em sigma recebe-se a norma_2 de cada coluna de A e nele retorna-se
+ * a diagonal principal de R. Em map retorna-se o vetor que representa as trocas de colunas
+ * feitas durante a decomposição. 'n' é o número de linhas da matriz e 'm' é o número de colunas */
+void qr(double A[][MAX], double b[], double sigma[], int map[], int n, int m) {
+    int i, j, k, maxI;
+    double max, alpha, beta;
 
-	for (j = 0; j < m; j++) {
-		/* Acha o máximo na coluna j a partr do indice i */
-		max = abs(A[j][j]);
-		for (i = j + 1; i < n; i++)
-			if (abs(A[i][j]) > max)
-				max = abs(A[i][j]);
+    for (j = 0; j < m; j++) {
+        /* swap columns */
+        max = sigma[j];
+        maxI = j;
+        for (k = j + 1; k < m; k++)
+            if (sigma[k] > max) {
+                max = sigma[k];
+                maxI = k;
+            }
+        if (maxI != j) {
+            swapColumns(A, n, j, maxI);
+            swap(&sigma[j], &sigma[maxI]);
+        }
+        map[j] = maxI;
 
-		/* Calcula a Q_i e o gama */
-		if (max == 0)
-			gama = 0;
-		else {
-			sigma = 0;
-			/* Norma2 da coluna j começando da linha i */
-			for (i = j; i < n; i++) {
-				A[i][j] /= max;
-				sigma += A[i][j] * A[i][j];
-			}
-			sigma = sqrt(sigma);
+        if (A[j][j] < 0)
+            sigma[j] = -sigma[j];
+        A[j][j] += sigma[j];
 
-			if (A[j][j] < 0)
-				sigma = -sigma;
-			A[j][j] += sigma;
+        /* Multiplica Q_j por A_j */
+        for (k = j + 1; k < m; k++) {
+            alpha = 0;
+            for (i = j; i < n; i++)
+                alpha += A[i][j] * A[i][k];
+            for (i = j; i < n; i++)
+                A[i][k] -= alpha * A[i][j] / (sigma[j] * A[j][j]);
+        }
+        
+        /* multiplica Q_j por b_j */        
+        beta = 0;
+        for (i = j; i < n; i++)
+            beta += A[i][j] * b[i];
+        for (i = j; i < n; i++)
+            b[i] -= beta * A[i][j] / (sigma[j] * A[j][j]);
 
-			gama = 1 / (sigma * A[j][j]);
-			sigma *= max;
-		}
-		
-		/* Multiplica Q_j por A_j */
-		for (k = j + 1; k < m; k++) {
-			alpha = 0;
-			for (i = j; i < n; i++)
-				alpha += A[i][j] * A[i][k];
-			for (i = j; i < n; i++)
-				A[i][k] -= gama * alpha * A[i][j];
-		}
-		
-		/* multiplica Q_j por b_j */		
-		beta = 0;
-		for (i = j; i < n; i++)
-			beta += A[i][j] * b[i];
-		for (i = j; i < n; i++)
-			b[i] -= gama * beta * A[i][j];
+        sigma[j] = -sigma[j];
 
-		A[j][j] = -sigma;
-	}
+        /* Reajuste das novas normas2 de cada coluna a partir da linha j + 1 */
+        for (k = j + 1; k < m; k++)
+            sigma[k] = sqrt(sigma[k] * sigma[k] - A[j][k] * A[j][k]);
+    }
 }
 
-void qr_solve(double A[][MAX], double b[], int m) {
+/* Resolve Ax = b, dado a decomposição QR de A.
+ * Em A recebe-se a matriz que contem as Q_i e R. Em b recebe-se o vetor c = Qt * b.
+ * Em sigma recebe-se o vetor que contém a diagonal principal de R. m é o número de 
+ * colunas de A.
+ * Calcula-se a partir disso o 'x' tal que Rx = c */
+void qr_solve(double A[][MAX], double b[], double sigma[], int m) {
     int i, j;
 
     for (i = m - 1; i >= 0; i--) {
         for (j = i + 1; j < m; j++)
             b[i] -= A[i][j] * b[j];
-        if (A[i][i] == 0)
-            printf("qr_solve: Division by 0");
-        b[i] /= A[i][i];
+        b[i] /= sigma[i];
     }
 }
 
-void prepareMatrix(double A[][MAX], int map[], int n, int m) {
-	int i, j, k;
-	double norm, maxNorm = -1; // impossible
-	int maxNormColIdx = -1; // impossible
+/* devolve em sigma as normas de cada coluna de A. A tem n linhas e m colunas */
+void getColNorms(double A[][MAX], double sigma[], int n, int m) {
+    int i, j;
+    double max, normalized;
+    
+    for (j = 0; j < m; j++) {
+        /* Norma infinita */
+        max = abs(A[1][j]);
+        for (i = 1; i < n; i++)
+            if (abs(A[i][j] > max))
+                max = abs(A[i][j]);
 
-	for (k = 0; k < m; k++) {
-		maxNorm = 0;
-		maxNormColIdx = k;
-		for (j = k; j < m; j++) {
-			norm = 0;
-			for (i = k; i < n; i++) 
-				norm += A[i][j] * A[i][j];
-			if (norm > maxNorm) {
-				maxNorm = norm;
-				maxNormColIdx = j;
-			}
-		}
-		
-		if (maxNormColIdx != k) 
-			swapColumns(A, n, maxNormColIdx, k);
-
-		map[k] = maxNormColIdx;
-	}
+        /* Norma2 da coluna j */
+        for (i = 0; i < n; i++) {
+            normalized = A[i][j] / max;
+            sigma[j] += normalized * normalized;
+        }
+        sigma[j] = sqrt(sigma[j]);
+        sigma[j] *= max;            
+    }
 }
 
 /** FUNÇÕES AUXILIARES *****************************************************************/
-
+/* Lê uma matriz A da stdin no seguinte formato:
+ * n m
+ * linha coluna elemento
+ * linha coluna elemento
+ * ... */
 int readMatrix(double A[][MAX], double b[], int *n, int *m) {
-	int k, i, j, nm;
-	scanf("%d", n);
-	scanf("%d", m);
-	nm = *n * *m;
-	
-	// Clear A and b
-	for (i = 0; i < *n; i++) {
-		for (j = 0; j < *m; j++)
-			A[i][j] = 0;
-		b[i] = 0;
-	}
+    int k, i, j, nm;
+    scanf("%d", n);
+    scanf("%d", m);
+    nm = *n * *m;
+    
+    // Clear A and b
+    for (i = 0; i < *n; i++) {
+        for (j = 0; j < *m; j++)
+            A[i][j] = 0;
+        b[i] = 0;
+    }
 
-	for (k = 0; k < nm; k++) {
-		scanf("%d %d", &i, &j);
-		scanf("%lf", &A[i][j]);
-	}
-	for (k = 0; k < *n; k++) {
-		scanf("%d", &i);
-		scanf("%lf", &b[i]);
-	}
+    for (k = 0; k < nm; k++) {
+        scanf("%d %d", &i, &j);
+        scanf("%lf", &A[i][j]);
+    }
+    for (k = 0; k < *n; k++) {
+        scanf("%d", &i);
+        scanf("%lf", &b[i]);
+    }
 }
 
+/* A partir de um vetor map, calcula-se as novas posições dos elementos de b */
 void remap(double b[], int map[], int n) {
-	int i;
-	for (i = 0; i < n; i++)
-		swap(&b[i], &b[map[i]]);
+    int i;
+    for (i = n - 1; i >= 0; i--)
+        swap(&b[i], &b[map[i]]);
 }
 
+/* imprime a matriz A */
 void printMatrix(double A[][MAX], int n, int m) {
-	int i, j;
+    int i, j;
 
-	printf("\nA = \n");
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < m; j++)
-			printf("%lf ", A[i][j]);
-		printf("\n");
-	}
-	printf("\n");
+    printf("\nA = \n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++)
+            printf("%lf ", A[i][j]);
+        printf("\n");
+    }
+    printf("\n");
 }
 
+/* Imprime o vetor b */
 void printVector(double b[], int n) {
     int i;
     printf("\nb = \n");
@@ -187,16 +191,18 @@ void printVector(double b[], int n) {
     printf("\n");
 }
 
+/* Troca a coluna col1 pela col2 de A */
 void swapColumns(double A[][MAX], int n, int col1, int col2) {
-	int i;
+    int i;
 
-	for (i = 0; i < n; i++) {
-		swap(&A[i][col1], &A[i][col2]);
-	}
+    for (i = 0; i < n; i++) {
+        swap(&A[i][col1], &A[i][col2]);
+    }
 }
 
+/* Troca as variaveis a e b */
 void swap(double *a, double *b) {
-	double temp = *a;
-	*a = *b;
-	*b = temp;
+    double temp = *a;
+    *a = *b;
+    *b = temp;
 }
---
+++ qr/studies/qr.c
@@ -0,0 +1,202 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 100
+#define abs(x) (((x) < 0) ? -(x) : (x))
+
+/** FUNÇÕES PRINCIPAIS **/
+void qr(double A[][MAX], double b[], int n, int m);
+void qr_solve(double A[][MAX], double b[], int m);
+void prepareMatrix(double A[][MAX], int map[], int n, int m);
+
+/** FUNÇÕES AUXILIARES **/
+int readMatrix(double A[][MAX], double b[], int *n, int *m); 
+void remap(double b[], int map[], int n);
+void printMatrix(double A[][MAX], int n, int m);
+void printVector(double b[], int n);
+void swapColumns(double A[][MAX], int n, int col1, int col2);
+void swap(double *a, double *b); 
+
+int main(int argc, char **argv) {
+	int n, m;
+	double A[MAX][MAX];
+	double b[MAX];
+	int map[MAX];
+
+	readMatrix(A, b, &n, &m);
+
+//	printMatrix(A, n, m);
+//	system("sleep 5");
+	prepareMatrix(A, map, n, m);
+//	printMatrix(A, n, m);
+//	system("sleep 10");
+
+//  printMatrix(A, n, m);
+//  printVector(b, n);
+
+	qr(A, b, n, m);
+    qr_solve(A, b, m);
+	remap(b, map, n);
+
+//  printMatrix(A, n, m);
+    printVector(b, n);
+
+	return 0;
+}
+
+/** FUNÇÕES PRINCIPAIS *****************************************************************/ 
+
+void qr(double A[][MAX], double b[], int n, int m) {
+	int i, j, k;
+	double max, sigma, alpha, beta, gama;
+
+	for (j = 0; j < m; j++) {
+		/* Acha o máximo na coluna j a partr do indice i */
+		max = abs(A[j][j]);
+		for (i = j + 1; i < n; i++)
+			if (abs(A[i][j]) > max)
+				max = abs(A[i][j]);
+
+		/* Calcula a Q_i e o gama */
+		if (max == 0)
+			gama = 0;
+		else {
+			sigma = 0;
+			/* Norma2 da coluna j começando da linha i */
+			for (i = j; i < n; i++) {
+				A[i][j] /= max;
+				sigma += A[i][j] * A[i][j];
+			}
+			sigma = sqrt(sigma);
+
+			if (A[j][j] < 0)
+				sigma = -sigma;
+			A[j][j] += sigma;
+
+			gama = 1 / (sigma * A[j][j]);
+			sigma *= max;
+		}
+		
+		/* Multiplica Q_j por A_j */
+		for (k = j + 1; k < m; k++) {
+			alpha = 0;
+			for (i = j; i < n; i++)
+				alpha += A[i][j] * A[i][k];
+			for (i = j; i < n; i++)
+				A[i][k] -= gama * alpha * A[i][j];
+		}
+		
+		/* multiplica Q_j por b_j */		
+		beta = 0;
+		for (i = j; i < n; i++)
+			beta += A[i][j] * b[i];
+		for (i = j; i < n; i++)
+			b[i] -= gama * beta * A[i][j];
+
+		A[j][j] = -sigma;
+	}
+}
+
+void qr_solve(double A[][MAX], double b[], int m) {
+    int i, j;
+
+    for (i = m - 1; i >= 0; i--) {
+        for (j = i + 1; j < m; j++)
+            b[i] -= A[i][j] * b[j];
+        if (A[i][i] == 0)
+            printf("qr_solve: Division by 0");
+        b[i] /= A[i][i];
+    }
+}
+
+void prepareMatrix(double A[][MAX], int map[], int n, int m) {
+	int i, j, k;
+	double norm, maxNorm = -1; // impossible
+	int maxNormColIdx = -1; // impossible
+
+	for (k = 0; k < m; k++) {
+		maxNorm = 0;
+		maxNormColIdx = k;
+		for (j = k; j < m; j++) {
+			norm = 0;
+			for (i = k; i < n; i++) 
+				norm += A[i][j] * A[i][j];
+			if (norm > maxNorm) {
+				maxNorm = norm;
+				maxNormColIdx = j;
+			}
+		}
+		
+		if (maxNormColIdx != k) 
+			swapColumns(A, n, maxNormColIdx, k);
+
+		map[k] = maxNormColIdx;
+	}
+}
+
+/** FUNÇÕES AUXILIARES *****************************************************************/
+
+int readMatrix(double A[][MAX], double b[], int *n, int *m) {
+	int k, i, j, nm;
+	scanf("%d", n);
+	scanf("%d", m);
+	nm = *n * *m;
+	
+	// Clear A and b
+	for (i = 0; i < *n; i++) {
+		for (j = 0; j < *m; j++)
+			A[i][j] = 0;
+		b[i] = 0;
+	}
+
+	for (k = 0; k < nm; k++) {
+		scanf("%d %d", &i, &j);
+		scanf("%lf", &A[i][j]);
+	}
+	for (k = 0; k < *n; k++) {
+		scanf("%d", &i);
+		scanf("%lf", &b[i]);
+	}
+}
+
+void remap(double b[], int map[], int n) {
+	int i;
+	for (i = 0; i < n; i++)
+		swap(&b[i], &b[map[i]]);
+}
+
+void printMatrix(double A[][MAX], int n, int m) {
+	int i, j;
+
+	printf("\nA = \n");
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++)
+			printf("%lf ", A[i][j]);
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void printVector(double b[], int n) {
+    int i;
+    printf("\nb = \n");
+    for (i = 0; i < n; i++)
+        printf("%lf\n", b[i]);
+    printf("\n");
+}
+
+void swapColumns(double A[][MAX], int n, int col1, int col2) {
+	int i;
+
+	for (i = 0; i < n; i++) {
+		swap(&A[i][col1], &A[i][col2]);
+	}
+}
+
+void swap(double *a, double *b) {
+	double temp = *a;
+	*a = *b;
+	*b = temp;
+}
