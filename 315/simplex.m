3ad5dcf21a22cad0942248ca723edb9097952a6f
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2015-05-10 20:19:23 -0300

    Comentei uma parte do código que acho que está errada.

--- 315/simplex.m
+++ 315/simplex.m
@@ -1,42 +1,44 @@
 function simplex (A, b, c, m, n, x)
 
-	% Calcular indices basicos e não básicos a partir de x
-	j = 1;
-	k = 1;
-	I = struct('b', [], 'n', []);
-	for i = 1 : n
-		if x(i) != 0
-			I.b(j++) = i;
-		else
-			I.n(k++) = i;
-		end
-	end
+    % Calcular indices basicos e não básicos a partir de x
+    j = 1;
+    k = 1;
 
-	assert(!(length(I.b) < m), "x é degenerado!");
-	assert(length(I.b) == m, "Base não tem m elementos!");
+    % I.b é o vetor de indices básicos e I.n é o vetor de índices não-básicos em x
+    I = struct('b', [], 'n', []);
+    for i = 1 : n
+        if x(i) != 0
+            I.b(j++) = i;
+        else
+            I.n(k++) = i;
+        end
+    end
 
-	% print resultados
-	printf("\nCalcula a base:\n");
-	x
-	I
-	
-	% Calcula cb e B
-	c = struct('c', c, 'b', []);
-	k = 1;
-	for i = I.b
-		c.b(k) = c.c(i);
-		B(:,k++) = A(:,i);
-	end
-	c.b = c.b';
+    assert(!(length(I.b) < m), "x é degenerado!");
+    assert(length(I.b) == m, "Base não tem m elementos!");
+    
+    % print resultados
+    printf("\nCalcula a base:\n");
+    x
+    I
+    
+    % Calcula cb e B
+    c = struct('c', c, 'b', []);
+    k = 1;
+    for i = I.b
+    c.b(k) = c.c(i);
+        B(:,k++) = A(:,i);
+    end
+    c.b = c.b';
 
-	% print resultados
-	printf("\nCalcula matriz basica B e custos basicos cb\n");
-	A
-	c.c
-	c.b
-	B
+    % print resultados
+    printf("\nCalcula matriz basica B e custos basicos cb\n");
+    A
+    c.c
+    c.b
+    B
 
-	% calcula custo reduzido de j, indice não básico, até achar um custo < 0 ou testar todos os indices
+    % calcula custo reduzido de j, indice não básico, até achar um custo < 0 ou testar todos os indices
     [redc, d, ij] = custoDirecao(A, B, c, n, m, I);
     [imin, teta] = calculaTeta(x, d, I);
     while redc < 0 && imin != -1
@@ -53,72 +55,84 @@ function simplex (A, b, c, m, n, x)
         [imin, teta] = calculaTeta(x, d, I);
     end
 
-	printf("\nFim:\n");
-	I.b
-	I.n
-	x
+    printf("\nFim:\n");
+    I.b
+    I.n
+    x
 end 
 
 %%%%%%%%%%%%%%% FUNÇÕES AUXILIARES %%%%%%%%%%%%%%%
 
 % Calcula a direção e custo reduzido a partir de um ponto x
-% retorna o o custo a direção e o indice ij de In de onde o custo
+% retorna o custo, a direção e o indice ij de In de onde o custo
 % reduzido passou a ser < 0
 function [redc, d, ij] = custoDirecao(A, B, c, n, m, I)
-	i = 1;
-	do
-		j = I.n(i);
-		printf("\nCalcula Custo reduzido referente a direção d%d\n", j);
-		d = direcaoViavel(A, inverse(B), n, m, j, I);
-		redc = custoReduzido(c, d, j)
-		i++;
+    i = 1;
+    do
+        j = I.n(i);
+        printf("\nCalcula Custo reduzido referente a direção d%d\n", j);
+        d = direcaoViavel(A, inverse(B), n, m, j, I);
+        redc = custoReduzido(c, d, j)
+        i++;
 
-		d.d	
-	until (redc < 0) || (i > n - m)
-	ij = i - 1 % Indice de j em In
+        d.d 
+    until (redc < 0) || (i > n - m)
+    ij = i - 1 % Indice de j em In
 end
 
-% Calcula a j-ésima direção viável: -B^-1 * A_j
+% Calcula a j-ésima direção viável: 
+% d.b = -B^-1 * A_j
+% d.d = (0,...,0) + ej + e_B(1)
 function d = direcaoViavel(A, invB, n, m, j, I)
-	d = struct('d', [], 'b', []);
+    d = struct('d', [], 'b', []);
 
-	d.d = zeros(n, 1);
-	d.d(j) = 1;
+    d.d = zeros(n, 1);
+    d.d(j) = 1;
 
-	for k = 1 : m
-		d.d(I.b(k)) = -invB(k, :) * A(:, j);
-		d.b(k) = d.d(I.b(k));
-	end
+    %for k = 1 : m
+    %    d.d(I.b(k)) = -invB(k, :) * A(:, j);
+    %    d.b(k) = d.d(I.b(k));
+    %end
+    %alternativa (não sei se vale mais a pena)
+    d.b = -invB * A(:,j);
+    for k = 1 : m
+        d.d(I.b(k)) = d.b(k);
+    end
 end
 
 % Calcula o custo reduzido: c_j - c.b' * B^-1 * A_j
 function redc = custoReduzido(c, d, j)
-	redc = c.c(j) + c.b' * d.b;
+    redc = c.c(j) + c.b' * d.b;
 end
 
 % calcula o teta: min{ -x_b(i) / d_b(i) }, d_b(i) < 0, i em Ib
 % além de retornar teta, retorna o índice de Ib que minimiza a expressão acima
 function [imin, teta] = calculaTeta(x, d, I) 
-	i = 1;
+    i = 1;
 
-	% Verifica se existe um d_B(i) < 0
-	while i <= length(I.b) && d.d(I.b(i)) >= 0
-		i++;
-	end
+    % Verifica se existe um d_B(i) < 0
+    while i <= length(I.b) && d.d(I.b(i)) >= 0
+        i++;
+    end
 
-	if i <= length(I.b)
-		teta = -x(I.b(i)) / d.d(I.b(i));
-		imin = I.b(i++);
-		while(i < length(I.b))
-			t = -x(I.b(i)) / d.d(I.b(i));
-			if t < teta
-				teta = t;
-				imin = I.b(i);
-			end
-			i++;
-		end
-	else
-		imin = -1;
-		teta = 0;
-	end
+    if i <= length(I.b)
+        teta = -x(I.b(i)) / d.d(I.b(i));                                                  
+        imin = I.b(i++);                     
+        while(i < length(I.b))
+            t = -x(I.b(i)) / d.d(I.b(i));       % conseguimos garantir que esse d.d(I.b(i)) é menor que zero?
+                                                % só queremos olhar para i básico em que di < 0.
+            if t < teta % && t >= 0             uma possivel solução
+                teta = t;
+                imin = I.b(i);
+            end
+            i++;
+        end
+    else
+        imin = -1;
+        teta = 0;
+    end
+     % tentativa 2
+     %
+     % while (i <= length (I.b))
+     %   
 end
