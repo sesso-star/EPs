22968df681c0ba4d683a6caf27b4ebf199112727
Bruno Sesso <bruno.sesso@usp.br>
2015-06-10 16:56:34 -0300

    terminei?

--- 315/EP3/simplex.m
+++ 315/EP3/simplex.m
@@ -1,102 +1,100 @@
-function [ind, v] = simplex (A, b, c, m, n)
-    [A, b, invB, I, m] = phase1(A, b, c, m, n)
-    %[ind, v] = phase2(A, b, c, m, n, x, I, invB)
-end
-
-function [A, b, invB, I, rankA] =  phase1 (A, b, c, m, n)
-    printf("Fase 1\n\n") ;
+function [ind, v] = simplex(A, b, c, m, n)
+    % Arruma restrições para que b > 0
     for i = 1 : m
         if (b < 0)
             b *= -1;
             A(i, :)  *= -1;
         end
     end
-    A
-    A1 = [A, eye(m)]
-    c1 = [zeros(n, 1); ones(m, 1)]
-    % solução inicial x = 0, y = b
-    x1 = [zeros(n, 1); b]
-    I.b = [n + 1 : n + m]
-    I.n = [1 : n]
-    invB = inv(A1(:, I.b))
-    [ind, x, I] = phase2(A1, b, c1, m, n + m, x1, I, invB); 
-    % aqui precisamos verificar se todas variaveis articificiais valem zero. Se isso não
-    % aconteceu, temos que o problema inicial é inviável. 
-    [I, A, invB, m] = removeArtificials(x, I, A, invB, m, n);
+
+    % Cria problema auxiliar para achar primeira solução para o problema primal
+    A = [A, eye(m)];
+    x = [zeros(n, 1); b];
+    c1 = [zeros(n, 1); ones(m, 1)];
+    I = struct('b', [n + 1 : n + m], 'n', [1 : n]);
+    invB = inv(A(:, I.b));
+
+    % Resolve problema auxiliar
+    [ind, x, I, invB] = fase2(A, b, c1, m, n + m, x, I, invB);
+
+    % Verifica se o problema primal é viável
+    if x(n + 1 : n + m) != 0
+        ind = 1;
+        v = x = [];
+        return;
+    end
+
+    % Remove vaiáveis artificiais da base. (não altera x)
+    [I, A, invB, m] = removeArtificials(A, I, invB, m, n);
+    x = x(1 : n);
+
+    % Resolve problema primal, com solução encontrada
+    [ind, v, I, invB] = fase2(A, b, c, m, n, x, I, invB);
 end
 
-function [ind, v, I] = phase2 (A, b, c, m, n, x, I, invB)
-    printf("Iterando 0:\n")
-    printXb(x, I, m);
-    printCusto(x, c, n);
+function [ind, v, I, invB] = fase2(A, b, c, m, n, x, I, invB)
+    %
+    %
+    %
 
-    imin = 0;
-    it = 1;
     [redc, u, ij] = custoDirecao(A, invB, c, n, m, I);
-    while redc < 0 % se essa condição falha, x é ótimo
+    while redc < 0                              % se essa condição falha, x é ótimo
         [imin, teta] = calculaTeta(x, u, I);
-        if imin == -1 % custo ótimo é -inf e u tem a direção
+        if imin == -1                           % custo ótimo é -inf e u tem a direção
             ind = -1;
             v = u2d(u, I.n(ij), I);
-            printf("Solução é -inf na direção:\n");
             return;
         end
 
         % atualiza x
-        x = atualizax(x, teta, u, I.n(ij), I, m); 
+        x = atualizax(x, teta, u, I.n(ij), I); 
         [I, invB] = atualizaBase(I, invB, u, imin, ij, m); 
-        printf("Iterando %d:\n", it);
-        printXb(x, I, m);
-        printCusto(x, c, n);
 
-        it++;
         [redc, u, ij] = custoDirecao(A, invB, c, n, m, I);
     end
 
-    ind = 0;
     v = x;
-    printf("Solução ótima encontrada:\n");
-    v
+    ind = 0;
 end 
 
 
 %%%%%%%%%%%%%%% FUNÇÕES AUXILIARES %%%%%%%%%%%%%%%
 
 function [redc, u, ij] = custoDirecao(A, invB, c, n, m, I)
-    % Dado uma matriz A, a matriz Basica invertida e os indices básicos e não basicos
-    % essa função acha um indice ij de indices não basicos, o qual o indice associado
-    % gera uma direção onde o custo reduzido de I.n(ij) é < 0. 
-    % Retorna-se o custo reduzido de tal indice, os valores da direção de indices 
-    % basicos e o indice ij. Além disso, o índice ij é o menor indice não-básico com
-    % custo reduzino menor do que zero.
-    % 
-    % redc = c(j) - [(c.b)' * (invB)] * Aj
-    % u = (invB) * Aj
-    % ij é o ij-ésimo componente não básico que vai se tornar básico
-    % p = cb * invB
+    % Recebe:
+    %   A: Matriz de restrições
+    %   invB: Inversa da matriz B (colunas de A com indice básico)
+    %   c: Vetor de custos
+    %   n: número de variáveis
+    %   m: número de restrições
+    %   I: estrutura de indices básicos (I.b) e não básicos (I.n)
+    %
+    % Procura por uma direção a partir de uma solução (consegue-se isso, pois temos invB)
+    % na qual o custo reduzido (custo na solução encontrada a partir da direção) é menor 
+    % que zero.
+    %
+    % Retorna:
+    %   redc: custo reduzido associado à direção encontrada.
+    %   u: Direção na qual o custo reduzido é menor que 0. 
+    %   ij: indice tal que I.n(ij) == j e j é o indice associado à direção u.
+    %   Caso não haja custo reduzido menor que 0, retorna-se redc = 0, u = [], ij = -1
     %
     % Esta função é O(nm)
 
-    cbinvB = zeros (1, m);
-    for i = 1 : m
-        cbinvB += c(I.b(i)) * invB(i, :); % O(m^2)
-    end
+    % Calcula c_b' * B^-1 para evitar contas repetidas.   ~ O(m^2)
+    cbinvB = c(I.b)' * invB;
 
-    printf("Custos Reduzidos\n");
+    % itera pelos indices não básicos, procurando custo reduzido < 0.   ~ O(nm)
     j = 1;
-    while j <= n - m % O(nm - m²)
+    while j <= n - m
         redc = c(I.n(j)) - cbinvB * A(:, I.n(j));
 
         if redc < -1e-10
-            % Achou um custo reduzido < 0
             ij = j;
+            u = invB * A(:, I.n(ij)); % O(m^2)
 
-            % Calcula direção u
-            u = calculaDirecao(A, invB, I.n(ij));       % O(m^2)
-            printf("\nEntra na base: %d\n\n", I.n(ij));
-            printDir(u, I, m);
-            
-            return;
+            % Retorna o primeiro custo reduzido < 0. (Regra anti-ciclagem pelo menor indice)
+            return; 
         end
 
         j++;
@@ -108,6 +106,7 @@ function [redc, u, ij] = custoDirecao(A, invB, c, n, m, I)
     redc = 0;
 end
 
+
 function [imin, teta] = calculaTeta(x, u, I) 
     % Calcula o teta: min{ -x_b(i) / d_b(i) }, d_b(i) < 0, i em Ib. Além de retornar 
     % teta, retorna o menor índice de Ib que minimiza a expressão acima.
@@ -116,7 +115,7 @@ function [imin, teta] = calculaTeta(x, u, I)
     teta = inf;
     
     for i = 1 : length(I.b)
-        if u(i) > 1e-10 % u_i > 0
+        if u(i) > 1e-10 
             t = x(I.b(i)) / u(i);
             if t < teta
                 teta = t;
@@ -124,19 +123,6 @@ function [imin, teta] = calculaTeta(x, u, I)
             end
         end
     end
-    printf("Theta*\n%f\n\n", teta);
-    if (imin != -1)
-        printf("Sai da base: %d\n\n\n", I.b(imin));
-    end
-end
-
-
-function u = calculaDirecao(A, invB, j)
-    % Calcula a j-ésima direção viável. Devolve em u
-    % o vetor u = -db = B^-1 * A_j
-    %
-
-    u = invB * A(:,j);
 end
 
 
@@ -153,17 +139,15 @@ function [I, invB] = atualizaBase(I, invB, u, imin, ij, m)
             invB(i,:) += (-u(i) * invB(imin,:)) / u(imin);
         end
     end
+
     invB(imin,:) /= u(imin);
 end
 
 
 function x = atualizax (x, t, u, j, I)
     % Dados x, theta, u e a variável j que entrou na base, atualiza o x s.v.b.
-    % 
     
-    for i = 1 : length(I.b)
-        x(I.b(i)) -= t * u(i);
-    end
+    x(I.b) -= t * u;
     x(j) = t;
 end
 
@@ -189,13 +173,11 @@ function I = calculaBase(x, n, m);
 end
 
 
-function [I, A, invB, m] = removeArtificials(x, I, A, invB, m, n)
+function [I, A, invB, m] = removeArtificials(A, I, invB, m, n)
     % Dado uma base I da primeira fase do simplex, esta função remove da base os
     % índices das variáveis artificiais. Se for necessário, deleta linhas LD de A
     
-    artindex = I.b(I.b > n);
-    
-    for l = artindex
+    for l = I.b(I.b > n)
         k = 1;
         while ((k <= n - m) && abs(invB(l, :) * A(:, I.n(k))) <= 1e-10)  %% por que abs? o_O
             k++;
@@ -205,7 +187,7 @@ function [I, A, invB, m] = removeArtificials(x, I, A, invB, m, n)
             % Siginifica que B^-1(l, :) * Aj = 0 para todo j, isso significa que A tem
             % suas linhas LD. Ou seja, podemos remover uma de suas linhas. Vamos remover
             % a l-ésima linha.
-            m -= 1;
+            m--;
             A(l, :) = [];
         else
             % vamos trocar a base do indice l para j
@@ -213,6 +195,8 @@ function [I, A, invB, m] = removeArtificials(x, I, A, invB, m, n)
             [I, invB] = atualizaBase(I, invB, u, l, k, m);
         end
     end
+    A = A(:, 1 : n);
+    I.n(I.n > n) = [];
 end
 
 function d = u2d(u, j, I)
@@ -223,6 +207,7 @@ function d = u2d(u, j, I)
 
     d = zeros(1, length(I.b) + length(I.n));
     d(j) = 1;
+    
     for i = 1 : length(I.b)
         d(I.b(i)) = -u(i);
     end
