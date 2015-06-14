8a823c2d273f8accfdadb5359c0a5d0d03658cdb
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2015-06-14 13:43:06 -0300

    ex6 contem um teste com linhas LD em A

---
+++ 315/EP3/ex6.m
@@ -0,0 +1,9 @@
+% Este teste tem A com posto inclompleto
+A = [1, 1, 1, -1, 0;
+     1, 1, 1,  0, 1;
+     0, 0, 0,  1, 1]; % esta restrição é LD com as duas ultimas
+b = [1; 2; 1];
+c = -[1; 1; 0.5; 0; 0];
+m = 3;
+n = 5;
+
